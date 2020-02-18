4cc39728eac1bafb5160994ce692c131107ae901
wangguanran <191029260@qq.com>
2020-02-18 22:00:34 +0800

    modify OperateDatabase to Singleton mode

--- scripts/operate_database.py
+++ scripts/operate_database.py
@@ -14,6 +14,12 @@ class OperateDatabase(object):
     '''
     数据库操作类
     '''
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
     def __init__(self):
         log.debug("Operate_Database Im In")
